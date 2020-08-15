8c0eea41f130b04dbc878333dc9914b822cbd012
Peter Pimley <peter.pimley@unity3d.com>
2020-08-15 13:40:09 +0100

    Simple reporting via Debug.Log

---
+++ Runtime/ZombieObjectDetector_Report_TTY.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Linq;
+
+
+namespace CSharpZombieDetector
+{
+
+
+
+	public class ZombieObjectDetector_Report_TTY : MonoBehaviour
+	{
+
+		private event System.Action Destroyed;
+
+		private void Awake()
+		{
+			var zod = GetComponent<ZombieObjectDetector>();
+			zod.ZombieHit += PrintHit;
+			Destroyed += () => zod.ZombieHit -= PrintHit;
+		}
+
+		private void OnDestroy()
+		{
+			if (Destroyed != null )
+				Destroyed();
+		}
+
+
+		private void PrintHit(ZombieObjectDetector.ZombieHitInfo info)
+		{
+			StringWriter w = new StringWriter();
+			w.WriteLine($"Found zombie object of type {info.obj.GetType()}");
+			w.WriteLine($"Member chain is:");
+			w.WriteLine(info.memberChain.Last().DeclaringType);
+			foreach (var m in info.memberChain.Reverse())
+				w.WriteLine($"-> {m}");
+			Debug.Log(w.ToString());
+		}
+	}
+
+
+}
